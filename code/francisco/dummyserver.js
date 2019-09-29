560e39907eb200f44148b6bb70e7706c19cf0dc9
fransicosuca <franciscosusana2292@gmail.com>
2019-09-29 23:31:27 +0200

    server file name update

--- code/francisco/dummyserver.js
+++ code/francisco/dummyserver.js
@@ -3,14 +3,14 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req,res)=>{
-    console.log("Request from: " + req.url)
-    console.log(res.socket.remoteAddress + '\n' + res.socket.remotePort)
+    console.log('Request from: ' + req.url)
+    console.log('Server address: ' + res.socket.remoteAddress + '\n' + 'Server port: ' + res.socket.remotePort)
     if(req.url === "/home" || req.url === "/"){
         res.writeHead(200,{'Content-Type':'text/html'})
-        fs.createReadStream(__dirname + "/index.html").pipe(res)
-    }else if(req.url === "/contacts"){
+        fs.createReadStream(__dirname + '/index.html').pipe(res)
+    }else if(req.url === '/contacts'){
         res.writeHead(200,{'Content-Type':'text/html'})
-        fs.createReadStream(__dirname + "/contacts.html").pipe(res)
+        fs.createReadStream(__dirname + '/contacts.html').pipe(res)
     }else if(req.url === "/api"){
         res.writeHead(200,{'Content-Type':'application/json'})
         let students = [{name:'Cesar',age:12},{name:'Oscar',age:14}]
@@ -21,4 +21,4 @@ const server = http.createServer((req,res)=>{
     }
 })
 server.listen(5100)
-console.log("Listening port 5100")
+console.log("Listening port 5100\n")
